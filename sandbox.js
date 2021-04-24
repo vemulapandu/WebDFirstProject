be477edc01c17940fef59fb90b582dea41d78cd1
vemulapandu <mallik.prabhanjan@gmail.com>
2021-04-25 00:09:45 +0530

    Added javascript file

---
+++ sandbox.js
@@ -0,0 +1,14 @@
+const popup = document.querySelector('.login-popup');
+const popbutton = document.querySelector('.login_button');
+const span = document.querySelector('.close');
+popbutton.addEventListener('click',function(){
+    popup.style.display = "block";
+});
+span.addEventListener('click',function(){
+    popup.style.display = "none";
+});
+window.addEventListener('click',function(event){
+    if(event.target==popup){
+        popup.style.display = "none";
+    }
+});
